1c5989f6fc415bc0d0dd4a4e3cadc10a5547db28
Pavel Zdor <pavel1zd@gmail.com>
2026-07-22 14:51:52 +0000

    fix

--- srv/patient-service.js
+++ srv/patient-service.js
@@ -1,29 +1,30 @@
 const cds = require('@sap/cds');
-
 module.exports = cds.service.impl(async function () {
 
     const { Diagnoses } = this.entities;
 
-    this.before('READ', Diagnoses, async req => {
+    this.before('READ', Diagnoses, async (req) => {
 
-        const patient = await SELECT.one
+        const email = req.user.attr?.email || req.user.email;
+
+        let patient = await SELECT.one
             .from('healthcare.Patient')
-            .where({
-                userId: req.user.id
-            });
+            .where({ userId: req.user.id });
+
+        if (!patient && email) {
+            patient = await SELECT.one
+                .from('healthcare.Patient')
+                .where({ email });
+        }
 
         if (!patient) {
             return req.reject(403, 'Patient not found');
         }
 
         req.query.where([
-            {
-                ref: ['patient_ID']
-            },
+            { ref: ['patient_ID'] },
             '=',
-            {
-                val: patient.ID
-            }
+            { val: patient.ID }
         ]);
 
     });
